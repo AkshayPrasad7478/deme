da59a3550b93b5331636821ce4400efc725a7dd8
AkshayPrasad7478 <165766505+AkshayPrasad7478@users.noreply.github.com>
2024-11-18 16:11:30 +0530

    Add files via upload

---
+++ demo/index.js
@@ -0,0 +1,21 @@
+const arr=[
+    {
+name: "Akshay",
+age: 27
+},
+
+{
+name: "Suraj",
+age: 22
+},
+
+{
+    name:"Sunny",
+    age:20
+}];
+
+const name1 = arr.map(obj=>
+ obj.age*2
+);
+
+console.log(name1);
